217ec51602b2ae67041e562728dde331be624750
Oksana Rubel <okhymych480@gmail.com>
2025-09-18 17:03:27 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-// write your code here
+const rows = document.querySelectorAll('thead tr, tbody tr, tfoot tr');
+
+rows.forEach((row) => {
+  const clone = row.children[1].cloneNode(true);
+
+  row.insertBefore(clone, row.children[row.children.length - 1]);
+});
