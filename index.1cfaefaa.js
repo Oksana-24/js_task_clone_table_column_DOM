fcd6625ce8e60fdb037103306f9b1954a3e3d74f
Oksana Rubel <okhymych480@gmail.com>
2025-09-18 17:39:42 +0300

    make build

---
+++ index.1cfaefaa.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("thead tr, tbody tr, tfoot tr").forEach(function(e){if(!(e.cells.length<2)&&!e.querySelector('[data-cloned-from="2"]')){var t=e.cells[1].cloneNode(!0);t.setAttribute("data-cloned-from","2"),e.insertBefore(t,e.children[e.children.length-1])}});
+//# sourceMappingURL=index.1cfaefaa.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const rows = document.querySelectorAll("thead tr, tbody tr, tfoot tr");
+rows.forEach((row)=>{
+    if (row.cells.length < 2) return;
+    if (row.querySelector('[data-cloned-from="2"]')) return;
+    const clone = row.cells[1].cloneNode(true);
+    clone.setAttribute("data-cloned-from", "2");
+    row.insertBefore(clone, row.children[row.children.length - 1]);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
